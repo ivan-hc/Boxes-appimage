451818f57412ad64f63b4fa1105ece5491afee2b
iVAN <88724353+ivan-hc@users.noreply.github.com>
2023-05-23 03:23:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat list.txt | ./appimagen gnome-boxes gsettings-desktop-schemas gsettings-backend xapps-common qemu-system-x86 tracker-miner-fs
+cat list.txt | ./appimagen gnome-boxes gsettings-desktop-schemas gsettings-backend xapps-common qemu-system-x86 libgtk-3-common libgtk-4-common
