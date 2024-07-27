eb8c6559d964a696cbc7218bc22a98fefb47a587
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-07-27 21:39:59 +0200

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -33,9 +33,9 @@ devel_pkgs="base-devel git meson mingw-w64-gcc cmake"
 # Apart from packages from the official Arch repos, you can also specify
 # packages from the Chaotic-AUR repo
 export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
-	ttf-dejavu ttf-liberation xorg-xwayland wayland xdg-user-dirs libusb \
+	which ttf-dejavu ttf-liberation xorg-xwayland wayland xdg-user-dirs \
 	xorg-server xorg-apps curl gnome-boxes qemu-desktop hicolor-icon-theme \
-	gtk3 gtk4 libadwaita xapp libvirt"
+	gtk3 gtk4 libadwaita xapp libvirt libusb"
 
 # If you want to install AUR packages, specify them in this variable
 export aur_packagelist=""
