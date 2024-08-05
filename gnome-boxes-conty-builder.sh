ae51df9321e1f2ef0f8860d345674ce85b5d9e10
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-05 05:07:06 +0200

    Update gnome-boxes-conty-builder.sh

--- gnome-boxes-conty-builder.sh
+++ gnome-boxes-conty-builder.sh
@@ -350,6 +350,6 @@ cd .. || exit 1
 
 # EXPORT THE APPDIR TO AN APPIMAGE
 VERSION=$(curl -Ls https://archlinux.org/packages/extra/x86_64/gnome-boxes/ | grep gnome-boxes | head -1 | tr ' ' '\n' | grep "^[0-9]")
-ARCH=x86_64 VERSION="$VERSION-1" ./appimagetool -s ./"$APP".AppDir
+ARCH=x86_64 VERSION="$VERSION" ./appimagetool -s ./"$APP".AppDir
 cd .. && mv ./tmp/*.AppImage ./Boxes-"$VERSION"-x86_86.AppImage || exit 1
 
